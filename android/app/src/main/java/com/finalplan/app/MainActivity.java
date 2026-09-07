759735129bb825018d1167993a9a6377c35d68b7
mlutf7-source <mlutf7@gmail.com>
2026-09-07 21:09:44 +0300

    apk

---
+++ android/app/src/main/java/com/finalplan/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.finalplan.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.finalplan.app',
+  appName: 'Finalplan',
+  webDir: 'dist'
+};
+
+export default config;
