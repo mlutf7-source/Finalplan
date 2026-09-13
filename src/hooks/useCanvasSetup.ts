85673c22b7203c136dacdf3f8b61ff7cbde875df
mlutf7-source <mlutf7@gmail.com>
2026-09-13 21:08:58 +0300

    Update useCanvasSetup.ts

--- src/hooks/useCanvasSetup.ts
+++ src/hooks/useCanvasSetup.ts
@@ -45,7 +45,7 @@ export function useCanvasSetup({
   const drawing = useDrawing(walls, axes, (w) => onWallsChange([...walls, w]));
 const edit = useWallEditor(walls, onWallsChange as any, axes);
   const dimensionMode = useDimensionMode(walls, axes, onAddDimension);
-  const dimensionEdit = useDimensionEdit(dimensions, walls, onDimensionsChange);
+  const dimensionEdit = useDimensionEdit(dimensions, walls, axes, onDimensionsChange);
   const elementEdit = useElementEdit(columns, windows, doors, walls, updateColumn, updateWindow, updateDoor, axes);  const textEdit = useTextEdit(texts, onUpdateText, onDeleteText, onAddText);
   const stairEdit = useStairEdit();
   const northArrowEdit = useNorthArrowEdit(northArrows, updateNorthArrow);
